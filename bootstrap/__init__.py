3a54d191a22b266fae683a8edf03895cbd25add4
almlys <almlys@1d0ef2df-3fe9-40e7-8648-ba97643a2676>
2008-01-28 17:18:26 +0000

    Some __init__'s to make packages
    git-svn-id: http://172.26.0.2/svn/sevendeset/7d7/trunk@32 1d0ef2df-3fe9-40e7-8648-ba97643a2676

---
+++ bootstrap/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#    sd7 Project BootStrap
+#    Copyright (C) 2008 Alberto Montañola Lacort
+#    Licensed under the GNU GPL. For full terms see the file COPYING.
+#
+#    Id: $Id$
+#
+
+"""
+Bootstrap app
+"""
+
+__version__ = "$Revision$"
+
