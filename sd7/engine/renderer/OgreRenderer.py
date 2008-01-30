762ee8bde81996cdcc2491bbd06618ee9b095a1f
Almlys <Almlys@1d0ef2df-3fe9-40e7-8648-ba97643a2676>
2008-01-30 01:00:34 +0000

    Setup code
    git-svn-id: http://172.26.0.2/svn/sevendeset/7d7/trunk@43 1d0ef2df-3fe9-40e7-8648-ba97643a2676

--- sd7/engine/renderer/OgreRenderer.py
+++ sd7/engine/renderer/OgreRenderer.py
@@ -28,6 +28,8 @@ class OgreRenderer(RendererInterface):
     _config = None #: Configuration dict (reference to global section)
     _initialized = False #: Is the render engine initialized?
     _logDir = "log" #: Path to the log folder
+    _renderWindow = None #: The render window
+    _sceneManager = None
 
     def __init__(self,options=None):
         if options!=None:
@@ -46,12 +48,16 @@ class OgreRenderer(RendererInterface):
             self._config["python.psyco"] = "enabled"
         if not self._config.has_key("python.psyco.log"):
             self._config["python.psyco.log"] = "enabled"
+        if not self._config.has_key("ogre.renderer"):
+            self._config["ogre.renderer"] = "OpenGL"
+        if not self._config.has_key("graphics.fullscreen"):
+            self._config["graphics.fullscreen"] = "false"
+        if not self._config.has_key("graphics.width"):
+            self._config["graphics.width"] = "800"
+        if not self._config.has_key("graphics.height"):
+            self._config["graphics.height"] = "600"
 
         ##self.config.set("global","ogre.config","1")
-        #self.config.set("global","graphics.fullscreen","0")
-        #self.config.set("global","graphics.width","800")
-        #self.config.set("global","graphics.height","600")
-        #self.config.set("global","ogre.renderer.direct3d","1")
         #self.config.set("global","ogre.core.enabled","1")
         #self.config.set("global","ogre.core.path","data/common/OgreCore.zip")
         #self.config.set("global","data.path","data/the_forge")
@@ -97,6 +103,36 @@ class OgreRenderer(RendererInterface):
             else:
                 print "Skipping Plugin %s..." %(plugin,)
 
+    def _configure(self):
+        """Create the render window depending of the configured settings"""
+        found = False
+        renList = self._root.getAvailableRenderers()
+        
+        print "Available render subsystems: "
+        for r in renList:
+            print r.getName()
+            if r.getName().startswith(self._config["ogre.renderer"]):
+                self._root.setRenderSystem(r)
+                found = True
+                print "%s found and set" %(r.getName(),)
+
+        if not found:
+            print "No Renderer was found in your system!"
+            return False
+        else:
+            self._root.initialise(False)
+            fullScreen = self._config["graphics.fullscreen"].lower() == "true"
+            w=int(self._config["graphics.width"])
+            h=int(self._config["graphics.height"])
+
+            self._renderWindow = self._root.createRenderWindow( "7d7 Engine", w, h, fullScreen)
+            return True
+
+    def _chooseSceneManager(self):
+        """Chooses a default SceneManager."""
+        self._sceneManager = self._root.createSceneManager(ogre.ST_GENERIC,"MainSceneManager")
+
+
     def _setUp(self):
         """Set up the renderer"""
         # We will manually load the plugins, and set the config
@@ -109,23 +145,24 @@ class OgreRenderer(RendererInterface):
         # The next option avoids flickering of the framerate stats
         self._root.setFrameSmoothingPeriod (5.0)
 
-        self._setUpResources()
+        ##self._setUpResources()
+        # Load configuration
         if not self._configure():
             return False
         
         self._chooseSceneManager()
         #?# self._createWorld()
-        self._createCamera()
-        self._createViewports()
+        #self._createCamera()
+        #self._createViewports()
 
         ogre.TextureManager.getSingleton().setDefaultNumMipmaps(5)
         #Set Anisotropic
-        ogre.MaterialManager.getSingleton().defaultAnisotropy = 8
+        ogre.MaterialManager.getSingleton().setDefaultAnisotropy(8)
         ogre.MaterialManager.getSingleton().setDefaultTextureFiltering(ogre.TFO_ANISOTROPIC)
 
-        self._createResourceListener()
-        self._loadResources()
+        #self._createResourceListener()
+        #self._loadResources()
 
-        self._createScene()
-        self._createFrameListener()
+        #self._createScene()
+        #self._createFrameListener()
         return True
