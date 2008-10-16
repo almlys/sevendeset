cf30a46a318b1683b2198921a1c45157d9a8ca8b
Almlys <Almlys@1d0ef2df-3fe9-40e7-8648-ba97643a2676>
2008-10-16 22:03:28 +0000

    Exporter code
    git-svn-id: http://172.26.0.2/svn/sevendeset/7d7/trunk@151 1d0ef2df-3fe9-40e7-8648-ba97643a2676

--- sd7/exporter/sd7Blender.py
+++ sd7/exporter/sd7Blender.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!BPY
 # -*- coding: utf-8 -*-
 #
 #    sd7 Project Engine
@@ -9,11 +9,65 @@
 #
 
 """
-Template file
+Name: 'sd7 Exporter (ogre)'
+Blender: 247
+Group: 'Export'
+Tooltip: 'sd7 Engine Exporter (Ogre renderer version)'
 """
 
+__author__ = "Alberto Montañola Lacort"
 __version__ = "$Revision$"
+__url__ = ['Author\' blog, http://almlys.org',
+           'sd7 Project page, http://7d7.almlys.org',
+           'sd7 Forum, http://forum.almlys.org']
+__bpydoc__ = "This script will export all objects from your scene to the format used by sd7"
+
 
 __all__ = []
 
+
+import sys
+
+try:
+    import Blender
+except ImportError:
+    print "This script only runs inside Blender"
+    sys.exit()
+
+from Blender import Registry
+
+from common.sdlogger import mlog
+
+
+class sd7Exporter(object):
+    """Main APP"""
+
+    #def __init__(self):
+        #self._config = Registry.GetKey("sd7Exporter")
+        #if self._config == None:
+        #    self._config = {}
+
+    def open_file(self,filename):
+        try:
+            import psyco
+            psyco.profile()
+        except ImportError:
+            print "Psyco not found"
+        self._start = time.clock()
+        print "Export to " + filename
+        self.export_scene(filename)
+
+    #def __del__(self):
+        #Registry.SetKey("
+
+
+if __name__ == '__main__':
+
+    exp = sd7Exporter()
+    
+    #fname = Blender.sys.makename(ext = ".7xml")
+
+    fname = "data/system/common/test.7xml"
+    
+    Blender.Window.FileSelector(exp.open_file, "Main description file", fname)
 
