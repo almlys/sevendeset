8d0db6e6f7891ba838ffacd395f211b7afe65226
Almlys <Almlys@1d0ef2df-3fe9-40e7-8648-ba97643a2676>
2009-06-22 23:50:26 +0000

    Launch Ball Master Terminator 5.1
    git-svn-id: http://172.26.0.2/svn/sevendeset/7d7/trunk@202 1d0ef2df-3fe9-40e7-8648-ba97643a2676

--- sd7/logic/world2.py
+++ sd7/logic/world2.py
@@ -20,8 +20,12 @@ from sd7.engine.controller import Controller
 from sd7.engine import Engine
 
 import random
+import time
 
 class World2(Controller):
+
+    _balli = 0
+    _last_shot = 0
 
     def __init__(self,params):
         Controller.__init__(self,params,"World")
@@ -46,15 +50,23 @@ class World2(Controller):
             for j in range(5):
                 self._worldMGR.createBox("box%ix%i" %(i,j), (j*20, i*25+10, 100), (20,20,20), 5)
 
-        ball = self._worldMGR.createBall("ball", (20, 70, 500), 15, 4)
-        ball.addForce((10,1000,-200000))
-        #self._worldMGR.createBox("box01",(200,300,200), (15,15,15), 200)
-
 
     def destroyScene(self):
         #self._worldMGR.destroyObject("ground")
         self._worldMGR.destroyAllObjects()
         
+
+    def onAction(self, name, down):
+        if down and name == "shot" and time.time()-self._last_shot > 0.5:
+            self._last_shot = time.time()
+            #pos = (20, 70, 500)
+            pos = Engine().getRenderer().getCamera().getRealPosition()
+            ori = Engine().getRenderer().getCamera().getRealDirection()
+
+            ball = self._worldMGR.createBall("ball%i" %(self._balli), pos, 15, 4)
+            self._balli += 1
+            ball.addForce(ori * 100000)
+
 
 
 
